dd356f0d975d75c0effaba85c31b3626f4ca9bff
Yorbit <marco.95112@hotmail.it>
2022-09-16 15:16:13 +0200

    end after fix vscode

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel primi passi</title>
+</head>
+<body>
+    <h1>HELLO WORLD</h1>
+    <h2>Laravel</h2>
+    <a href="/Home">Laravel</a>
+</body>
+</html>
